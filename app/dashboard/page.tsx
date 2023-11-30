5f800a70b52bcf8d3b74967b56c3239fb2286f58
adrianhh1426 <adrianhh1426@gmail.com>
2023-11-30 17:45:53 -0600

    usando server component un compooente con async await

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,10 @@
-export default function DashboardPage() {
+import { fetchRevenue } from "../lib/data";
+
+export default async function DashboardPage() {
+  const revenue = await fetchRevenue();
+
+  console.log("revenue: ", revenue)
+
   return (
     <h1>Dashboard Page</h1>
   );
