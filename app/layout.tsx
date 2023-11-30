8f0fe44f66afef96eca72eaa9e87855fac5b95b8
adrianhh1426 <adrianhh1426@gmail.com>
2023-11-30 13:50:11 -0600

    Agregando fuentes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
+import { monserrat } from './ui/fonts';
 import './ui/global.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <h1>Mi Layout</h1>
+      <body className={`${monserrat.className} antialiased`}>
         {children}
       </body>
     </html>
