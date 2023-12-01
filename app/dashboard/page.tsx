9579f179a756272e555a89dea8079b5a3cb115c4
adrianhh1426 <adrianhh1426@gmail.com>
2023-11-30 18:16:00 -0600

    mostrando datos en dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,25 @@
-import { fetchRevenue } from "../lib/data";
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import { lusitana } from "../ui/fonts";
 
 export default async function DashboardPage() {
   const revenue = await fetchRevenue();
-
-  console.log("revenue: ", revenue)
+  const latestInnvoices = await fetchLatestInvoices();
 
   return (
-    <h1>Dashboard Page</h1>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+
+      <div className="grid gap-6 sm:grid-cols-s2 lg:grid-cols-s4">
+      </div>
+
+      <div className="mt-6 grid grid-cols-s1 gap-6 md:grid-cols-s4 lg:grid-cols-s8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInnvoices} />
+      </div>
+    </main>
   );
 }
